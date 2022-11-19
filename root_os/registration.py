ecb986be559358b5b66a4ec15fadab710486ad49
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-19 21:45:43 +0100

    Made changes

--- root_os/registration.py
+++ root_os/registration.py
@@ -38,6 +38,4 @@ else:
     print("Your Serial Number is: " + config.serial_number)
     print("The Device Name is: " + config.device_name)
 
-print("test")
-
-subprocess.run(["ls"])
+subprocess.run(["echo \"test123\""])
